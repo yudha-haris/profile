a85da7ef895701d9b05e815c7aa49b1fd4ef8a3c
Yudha Haris Purnama <yudha@clade.ventures>
2023-08-17 20:49:03 +0700

    refactor: toggle dark mode button

--- components/ToggleDarkMode.js
+++ components/ToggleDarkMode.js
@@ -29,13 +29,11 @@ export default function ToggleDarkMode({ className }) {
     <button
       onClick={handleToggle}
       className={`${className} ${
-        isDarkMode ? "bg-blue-950" : "bg-blue-400"
-      }  rounded-2xl py-1 px-1 mx-6 my-4 lg:my-0 lg:mx-0 transition ease-in-out duration-300`}
+        isDarkMode ? "bg-blue-950 justify-start" : "bg-blue-400 justify-end"
+      } w-12 rounded-2xl py-1 px-1 mx-6 my-4 lg:my-0 lg:mx-0 transition ease-in-out duration-300`}
     >
-      {!isDarkMode && <div className="w-6" />}
       {isDarkMode && <MdDarkMode color="white" size={"24px"} />}
       {!isDarkMode && <MdLightMode color="yellow" size={"24px"} />}
-      {isDarkMode && <div className="w-6" />}
     </button>
   );
 }
