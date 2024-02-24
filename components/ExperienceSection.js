3e5d421cdb1d1e11b4341c9e6fe1c5cd97569388
Yudha Haris Purnama <yudha@clade.ventures>
2024-02-25 01:39:11 +0700

    feat: update jobs and profile

--- components/ExperienceSection.js
+++ components/ExperienceSection.js
@@ -12,53 +12,60 @@ export default function ExperienceSection() {
             </h4>
             <div className="w-full mt-16 lg:flex lg:flex-wrap">
               <ExperienceItem
+                img="/bbg.jpg"
+                title="Android Developer Intern"
+                company="Blue Bird Group"
+                companyUrl="https://www.bluebirdgroup.com/"
+                date="Februari 2024 -- Sekarang"
+              />
+              <ExperienceItem
                 img="/nbs.jpg"
                 title="Flutter Developer Intern"
                 company="Nusantara Beta Studio"
-                companyUrl="https://www.linkedin.com/company/nusantara-beta-studio/"
-                date="Agustus 2023 - Desember 2023"
+                companyUrl="https://nbs.co.id/"
+                date="Agustus 2023 -- Desember 2023"
               />
               <ExperienceItem
                 img="/toko-dizital.jpeg"
                 title="Android Developer"
                 company="Toko Dizital"
                 companyUrl="https://www.linkedin.com/company/toko-dizital/"
-                date="Mei 2023 - Juni 2023"
+                date="Mei 2023 -- Juni 2023"
               />
               <ExperienceItem
                 img="/sirka.jpeg"
                 title="Mobile Developer Intern"
                 company="Sirka"
                 companyUrl="https://www.linkedin.com/company/sirkaio/"
-                date="Nov 2022 - Mar 2023"
+                date="Nov 2022 -- Mar 2023"
               />
               <ExperienceItem
                 img="/clade.jpeg"
                 title="Software Engineer"
                 company="CLADE"
                 companyUrl="https://www.linkedin.com/company/cladetechnology/"
-                date="Mei 2022 - Okt 2022"
+                date="Mei 2022 -- Okt 2022"
               />
               <ExperienceItem
                 img="/fisiomotion.jpeg"
                 title="Mobile Application Developer"
                 company="FisioMotion"
                 companyUrl="https://www.linkedin.com/company/fisiomotion/"
-                date="Mar 2022 - Jun 2022"
+                date="Mar 2022 -- Jun 2022"
               />
               <ExperienceItem
                 img="/petanee-edited.png"
                 title="Mobile Application Developer"
                 company="Petanee (PT Ekosistem Agro Sejahtera)"
                 companyUrl="http://petanee.id/"
-                date="Nov 2021 - Mei 2022"
+                date="Nov 2021 -- Mei 2022"
               />
               <ExperienceItem
                 img="/tlq.png"
                 title="Mobile Application Developer"
                 company="The Living Quran Indonesia Foundation"
                 companyUrl="https://play.google.com/store/apps/details?id=com.main.tlquran"
-                date="Jul 2021 - Agu 2022"
+                date="Jul 2021 -- Agu 2022"
               />
             </div>
           </div>
