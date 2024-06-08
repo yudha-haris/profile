0c2061af0caf7c9b95b7cb1f4c03361780a2a036
Yudha Haris <yudha.haris@ui.ac.id>
2024-06-08 22:01:37 +0700

    refactor: move experiences data to json

--- components/Experience/ExperienceSection.js
+++ components/Experience/ExperienceSection.js
@@ -1,3 +1,4 @@
+import experiences from "../../data/Experience";
 import ExperienceItem from "./ExperienceItem";
 
 export default function ExperienceSection() {
@@ -6,67 +7,25 @@ export default function ExperienceSection() {
       <div className="container">
         <div className="w-full px-4">
           <div className="max-w-full mx-auto text-center mb-16">
-            <h2 className="font-bold text-white text-4xl mb-4">Working Experiences</h2>
+            <h2 className="font-bold text-white text-4xl mb-4">
+              Working Experiences
+            </h2>
             <h4 className="font-normal text-lg text-slate-300">
               I have write codes for these companies
             </h4>
             <div className="w-full mt-16 lg:flex lg:flex-wrap">
-              <ExperienceItem
-                img="/bbg.jpg"
-                title="Android Developer Intern"
-                company="Blue Bird Group"
-                companyUrl="https://www.bluebirdgroup.com/"
-                date="Feb 2024 -- Now"
-              />
-              <ExperienceItem
-                img="/nbs.jpg"
-                title="Flutter Developer Intern"
-                company="Nusantara Beta Studio"
-                companyUrl="https://nbs.co.id/"
-                date="Aug 2023 -- Dec 2023"
-              />
-              <ExperienceItem
-                img="/toko-dizital.jpeg"
-                title="Android Developer"
-                company="Toko Dizital"
-                companyUrl="https://www.linkedin.com/company/toko-dizital/"
-                date="May 2023 -- Jun 2023"
-              />
-              <ExperienceItem
-                img="/sirka.jpeg"
-                title="Mobile Developer Intern"
-                company="Sirka"
-                companyUrl="https://www.linkedin.com/company/sirkaio/"
-                date="Nov 2022 -- Mar 2023"
-              />
-              <ExperienceItem
-                img="/clade.jpeg"
-                title="Software Engineer"
-                company="CLADE"
-                companyUrl="https://www.linkedin.com/company/cladetechnology/"
-                date="May 2022 -- Oct 2022"
-              />
-              <ExperienceItem
-                img="/fisiomotion.jpeg"
-                title="Mobile Application Developer"
-                company="FisioMotion"
-                companyUrl="https://www.linkedin.com/company/fisiomotion/"
-                date="Mar 2022 -- Jun 2022"
-              />
-              <ExperienceItem
-                img="/petanee-edited.png"
-                title="Mobile Application Developer"
-                company="Petanee (PT Ekosistem Agro Sejahtera)"
-                companyUrl="http://petanee.id/"
-                date="Nov 2021 -- May 2022"
-              />
-              <ExperienceItem
-                img="/tlq.png"
-                title="Mobile Application Developer"
-                company="The Living Quran Indonesia Foundation"
-                companyUrl="https://play.google.com/store/apps/details?id=com.main.tlquran"
-                date="Jul 2021 -- Aug 2021"
-              />
+              {experiences.map((item) => {
+                return (
+                  <ExperienceItem
+                    key={item.title}
+                    img={item.img}
+                    title={item.title}
+                    company={item.company}
+                    companyUrl={item.companyUrl}
+                    date={item.date}
+                  />
+                );
+              })}
             </div>
           </div>
         </div>
---
+++ data/Experience/index.js
@@ -0,0 +1,60 @@
+const experiences = [
+    {
+        "img": "/bbg.jpg",
+        "title": "Android Developer Intern",
+        "company": "Blue Bird Group",
+        "companyUrl": "https://www.bluebirdgroup.com/",
+        "date": "Feb 2024 -- Now"
+    },
+    {
+        "img": "/nbs.jpg",
+        "title": "Flutter Developer Intern",
+        "company": "Nusantara Beta Studio",
+        "companyUrl": "https://nbs.co.id/",
+        "date": "Aug 2023 -- Dec 2023"
+    },
+    {
+        "img": "/toko-dizital.jpeg",
+        "title": "Android Developer",
+        "company": "Toko Dizital",
+        "companyUrl": "https://www.linkedin.com/company/toko-dizital/",
+        "date": "May 2023 -- Jun 2023"
+    },
+    {
+        "img": "/sirka.jpeg",
+        "title": "Mobile Developer Intern",
+        "company": "Sirka",
+        "companyUrl": "https://www.linkedin.com/company/sirkaio/",
+        "date": "Nov 2022 -- Mar 2023"
+    },
+    {
+        "img": "/clade.jpeg",
+        "title": "Software Engineer",
+        "company": "CLADE",
+        "companyUrl": "https://www.linkedin.com/company/cladetechnology/",
+        "date": "May 2022 -- Oct 2022"
+    },
+    {
+        "img": "/fisiomotion.jpeg",
+        "title": "Mobile Application Developer",
+        "company": "FisioMotion",
+        "companyUrl": "https://www.linkedin.com/company/fisiomotion/",
+        "date": "Mar 2022 -- Jun 2022"
+    },
+    {
+        "img": "/petanee-edited.png",
+        "title": "Mobile Application Developer",
+        "company": "Petanee (PT Ekosistem Agro Sejahtera)",
+        "companyUrl": "http://petanee.id/",
+        "date": "Nov 2021 -- May 2022"
+    },
+    {
+        "img": "/tlq.png",
+        "title": "Mobile Application Developer",
+        "company": "The Living Quran Indonesia Foundation",
+        "companyUrl": "https://play.google.com/store/apps/details?id=com.main.tlquran",
+        "date": "Jul 2021 -- Aug 2021"
+    }
+]
+
+export default experiences
